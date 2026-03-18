fd36e82703ea0ef43f49e4757121de3f30c7e1ca
Ethan Rivera <ethan.rivera@udem.edu>
2026-03-18 17:58:46 -0600

    chore: add Vercel Speed Insights to React entry point

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './app/App'
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import './styles/index.css'
 import './styles/theme.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <SpeedInsights />
   </React.StrictMode>,
 )
